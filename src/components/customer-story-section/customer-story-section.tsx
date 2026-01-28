6e7c9e8844c886c801d70f556637f2f5515285d0
IgoorToorres <igorevangelista121@gmail.com>
2026-01-28 10:02:20 -0300

    -feat / CallToAction

--- src/components/customer-story-section/customer-story-section.tsx
+++ src/components/customer-story-section/customer-story-section.tsx
@@ -6,7 +6,7 @@ const ptSansCaption = PT_Sans_Caption({
     weight: '700'
 })
 
-const customerStories = [
+const customerStory = [
     {
         content: 'Criar minha loja com o site.set foi a melhor decisão para o meu negócio. A plataforma é super intuitiva, e consegui colocar meus produtos à venda em poucos minutos.',
         author: {
@@ -25,7 +25,7 @@ const customerStories = [
     },
 ]
 
-export const customerStoriesSection = () => {
+export const CustomerStorySection = () => {
 
     return (
         <section className="container py-8 md:py-10">
@@ -36,18 +36,18 @@ export const customerStoriesSection = () => {
 
                 <div className="grid gap-32 md:grid-cols-2">
 
-                    {customerStories.map((customerStories) => (
-                        <div key={customerStories.author.name} className="flex flex-col gap-6 rounded-lg bg-gray-500 p-6 md:p-12">
+                    {customerStory.map((customerStory) => (
+                        <div key={customerStory.author.name} className="flex flex-col gap-6 rounded-lg bg-gray-500 p-6 md:p-12">
 
-                            <p className="text-balance text-gray-200">{customerStories.content}</p>
+                            <p className="text-balance text-gray-200">{customerStory.content}</p>
 
                             <div className="flex items-center gap-3">
 
                                 <div className="relative h-10 w-10 overflow-hidden rounded-full">
 
                                     <Image
-                                        src={customerStories.author.avatar}
-                                        alt={`${customerStories.author.name}-avatar`}
+                                        src={customerStory.author.avatar}
+                                        alt={`${customerStory.author.name}-avatar`}
                                         fill
                                         className="object-cover"
                                     />
@@ -55,8 +55,8 @@ export const customerStoriesSection = () => {
                                 </div>
 
                                 <div className="flex flex-col">
-                                    <strong className="text-gray-200 text-sm">{customerStories.author.name}</strong>
-                                    <span className="text-xs text-gray-300">{customerStories.author.role}</span>
+                                    <strong className="text-gray-200 text-sm">{customerStory.author.name}</strong>
+                                    <span className="text-xs text-gray-300">{customerStory.author.role}</span>
                                 </div>
 
                             </div>
