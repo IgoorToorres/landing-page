d35f3b3b1d258b5eb5c8bbb3649b34cfd10192ae
IgoorToorres <igorevangelista121@gmail.com>
2026-01-29 10:41:46 -0300

    fix - correção layout

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -20,7 +20,7 @@ const ptSansCaption = PT_Sans_Caption({
 
 export const Layout = ({ children }: LayoutProps) => {
     return (
-        <div className={`${inter.className} ${ptSansCaption.className} relative flex min-h-sreen flex-col bg-gray-700 font-inter`}>
+        <div className={`${inter.className} ${ptSansCaption.className} relative flex min-h-screen flex-col bg-gray-700 font-inter`}>
             <Header />
             <main className="flex-1 flex flex-col mt-10">
                 {children}
