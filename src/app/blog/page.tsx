4a275aca897bedccf46ed27f72e9cd4041bcc419
IgoorToorres <igorevangelista121@gmail.com>
2026-02-10 09:45:18 -0300

    feat - adicionado melhorias de SEO na pagina de blog e no layout global

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,14 +1,38 @@
 import { BlogList } from "@/templates/blog";
 import { allPosts } from "contentlayer/generated";
+import { Metadata } from "next";
 
-export default function BlogListPage(){
+
+export const metadata: Metadata = {
+    title: 'Site.Set',
+    description: 'Dicas e estratégias para impulsionar seu negócio',
+    robots: 'index, follow',
+    openGraph: {
+        title: 'Blog',
+        description: 'Dicas e estratégias para impulsionar seu negócio',
+        url: 'https://rocketseat-nextjs-fundamentals.vercel.app/og-image.jpg',
+        siteName: 'Site.Set',
+        locale: 'pt_BR',
+        type: 'website',
+        images: [
+            {
+                url: 'https://rocketseat-nextjs-fundamentals.vercel.app/og-image.jpg',
+                width: 800,
+                height: 600,
+                alt: 'Site.Set'
+            }
+        ]
+    }
+}
+
+export default function BlogListPage() {
 
     const sortedPosts = allPosts.sort(
         (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
     )
 
-    return(
-        <BlogList 
+    return (
+        <BlogList
             posts={sortedPosts}
         />
     )
